41f05560d47190dec536abfdddf1ab075b70efad
Melissa Trimarco <mtrimar1@depaul.edu>
2025-01-19 14:52:00 -0600

    Add files via upload
    [![Netlify Status](https://api.netlify.com/api/v1/badges/02c506d1-aaa3-4b9c-b738-043be9af292a/deploy-status)](https://app.netlify.com/sites/mels-react-basics/deploys)

---
+++ build/precache-manifest.104ed83cdef73da3d9a4c2f6b306dc2c.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b26ffa7022c04cc111cabe4f0493b4a0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "202441a214284fa6ec13",
+    "url": "/static/css/main.24e930ca.chunk.css"
+  },
+  {
+    "revision": "a7070d069324e15f337a",
+    "url": "/static/js/2.9737bc5f.chunk.js"
+  },
+  {
+    "revision": "fc0ba07c0f771d66c44444381376eaf8",
+    "url": "/static/js/2.9737bc5f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "202441a214284fa6ec13",
+    "url": "/static/js/main.9a7471a5.chunk.js"
+  },
+  {
+    "revision": "35f67668c217d61057f5",
+    "url": "/static/js/runtime-main.ed9d8f35.js"
+  },
+  {
+    "revision": "85d2a3c29be7f87aa53e1fb2756d8043",
+    "url": "/static/media/book-1.85d2a3c2.jpg"
+  },
+  {
+    "revision": "e3f2954ff2fa0d5f69347d17881acd44",
+    "url": "/static/media/book-2.e3f2954f.jpg"
+  },
+  {
+    "revision": "8d71286aabff94549973e0f2b7f560d1",
+    "url": "/static/media/book-3.8d71286a.jpg"
+  }
+]);
